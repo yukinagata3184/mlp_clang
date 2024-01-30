1c88fb40dbaa29162cea0728d67a099167640594
yukinagata3184 <yuki.nagata3184@gmail.com>
2024-01-30 17:28:39 +0900

    #2 [Change] Move the param a header file.

--- mlp_3layers.c
+++ mlp_3layers.c
@@ -1,47 +1,27 @@
 // 3 layers(input, hidden, output) MLP
 // auther:yukinagata3184
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-// Number of layers and neuron
-#define LAYER 3
-#define N1 2
-#define N2 2
-#define N3 1
-
-#define NUM_DATA 4
-#define LEARN_RATE 0.1
-#define MAX_LEARN 100
-
-// Initializing in the range of 0.0~1.0 for now. Actually, the -1.0~1.0 range might be better.
-#define RAND ((double)rand()/RAND_MAX)
+#include "param.h"
 
 // Number of connection each layer
 double x1[N1];
 double x2[N2];
 double x3[N3];
-
 // connection weight
 double w2[N2][N1];
 double w3[N3][N2];
-
 // bias
 double b2[N2];
 double b3[N3];
-
+// learn and infer data
 double input[NUM_DATA][N1] = {{0.0, 0.0}, {0.0, 1.0}, {1.0, 0.0}, {1.0, 1.0}};
 // XOR teacher signal
 double teacher[NUM_DATA][N3] = {{0.0}, {1.0}, {1.0}, {0.0}};
-
-void init(void);
 
 int main(void){
     srand(1);
     init();
 }
 
-// Initializing in the range of 0.0~1.0 for now. Actually, the -1.0~1.0 range might be better.
 void init(void){
     for(int j=0; j<N2; j++){
         for(int i=0; i<N1; i++){
---
+++ param.h
@@ -0,0 +1,24 @@
+#ifndef param
+#define param
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Number of layers and neuron
+#define LAYER 3
+#define N1 2
+#define N2 2
+#define N3 1
+// Number of learn and infer data
+#define NUM_DATA 4
+#define LEARN_RATE 0.1
+#define NUM_LEARN 100
+
+// Initializing in the range of 0.0~1.0 for now. Actually, the -1.0~1.0 range might be better.
+#define RAND ((double)rand()/RAND_MAX)
+
+// function prototypes
+void init(void);
+
+#endif
